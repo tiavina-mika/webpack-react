913ccb27108af71c29f6d2de33ffd5c5d68d45b2
tiavina-mika <ralainirinatiavina@gmail.com>
2020-04-03 13:07:03 +0300

    environment

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,48 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'home.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-                name: '[sha512:hash:base64:7].[ext]',
+module.exports = ({ env }) => {  
+    return {
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'home.bundle.js'
+        },
+        module: {
+          rules: [
+            {
+              test: /\.(js|jsx)$/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader"
+              }
             },
-          },
+            {
+              test: /\.html$/,
+              use: [
+                {
+                  loader: "html-loader"
+                }
+              ]
+            },
+            {
+              test: /\.(png|jpe?g|gif)$/i,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                      name: '[sha512:hash:base64:7].[ext]',
+                  },
+                },
+              ],
+            },
+          ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebPackPlugin({
+                template: "./public/index.html",
+                filename: "./index.html"
+            })
         ],
-      },
-    ]
-  },
-  plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebPackPlugin({
-          template: "./public/index.html",
-          filename: "./index.html"
-      })
-  ],
-  // devtool: "eval-cheap-module-source-map"
-};
+      devtool: env === 'dev'? "eval-cheap-module-source-map": "source-map"
+}};
