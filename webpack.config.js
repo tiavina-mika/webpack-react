4a675c2c6ec1c685b4a150c4bfc15627c8f2f310
tiavina-mika <ralainirinatiavina@gmail.com>
2020-04-03 11:45:09 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'home.bundle.js'
+  },
   module: {
     rules: [
       {
