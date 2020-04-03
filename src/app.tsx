ccd5293ef1ac955706fedb2d36fefe80499c95d1
tiavina-mika <ralainirinatiavina@gmail.com>
2020-04-03 19:01:35 +0300

    eslint

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent, ChangeEvent, useState, SyntheticEvent } from "react";
+import React, { FunctionComponent, ChangeEvent, useState, SyntheticEvent } from 'react';
+import PropTypes from 'prop-types';
 import logo from '../public/images/image.png';
 // export interface HelloProps { name: string; }
 // const Form = (props: HelloProps) => {
@@ -9,30 +10,30 @@ const Form: FunctionComponent<FormProps> = ({ name }) => {
     const [value, setValue] = useState<string>('');
     const [display, setDisplay] = useState<boolean>(false);
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement> )=> {
-        setValue(e.target.value)
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
     };
 
     const handleSubmit = (e: SyntheticEvent) => {
         e.preventDefault();
 
         if (value) setDisplay(true);
-    }
+    };
 
     return (
         <div>
-            <h1>Hello {name} </h1>
-            <img src={logo} alt="logo" style={{width: 200, height: 200}}/>
+            <h1>Hello {name}</h1>
+            <img src={logo} alt="logo" style={{ width: 200, height: 200 }} />
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={value}
-                    onChange={handleChange}
-                />
+                <input type="text" value={value} onChange={handleChange} />
             </form>
             {display && <p>{value}</p>}
         </div>
     );
-}
+};
+
+Form.propTypes = {
+    name: PropTypes.string.isRequired,
+};
 
 export default Form;
